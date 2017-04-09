fdf0035fcbf516de0c4172459d234f3edd5c2959
igor <hammerkid123@gmail.com>
2017-04-09 13:34:41 +0300

    flask application

--- db/sql_template.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `data` (
-  `name` varchar(50),
-  `value` varchar(50),
-  `date` varchar(50)
- 
-);
---
+++ wsgi.py
@@ -0,0 +1,5 @@
+from dev_ops_app import application
+
+if __name__ == "__main__":
+    application.run()
+
